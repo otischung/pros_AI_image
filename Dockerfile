ed493027ab377e8dd540687b9bb199f7cd199ee4
otischung <my072814638@gmail.com>
2024-01-30 12:27:02 +0800

    [FEAT] Add pros_car settings

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,23 @@
 FROM public.ecr.aws/paia-tech/pros-utils:20231121
 
+COPY ./requirements.txt /tmp
+RUN pip3 install -r /tmp/requirements.txt
 RUN apt update && \
-    apt install -y tmux ncdu
+    apt install -y libncurses5-dev libncursesw5-dev tmux screen ncdu
 
 RUN pip install --no-cache-dir torch torchvision torchaudio
 
+# Build your ROS packages
+# We use mount instead of copy
+# COPY ./src ${ROS2_WS}/src
+WORKDIR ${ROS2_WS}
+RUN . /opt/ros/humble/setup.sh && colcon build --event-handlers console_direct+ --cmake-args -DCMAKE_BUILD_TYPE=Release
+# You could decide wheather to delete source code
+
+RUN echo "source ${ROS2_WS}/install/setup.bash " >> /.bashrc 
+RUN echo "source /.bashrc  " >> ~/.bashrc 
+
+COPY ros_entrypoint.bash /ros_entrypoint.bash
+RUN chmod +x /ros_entrypoint.bash
 ENTRYPOINT [ "/ros_entrypoint.bash" ]
 CMD ["bash","-l"]
--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 #set -e
 
-function ros_source_env() 
-{
-	if [ -f "$1" ]; then
-		echo "sourcing   $1"
-		source "$1"
-	else
-		echo "notfound   $1"
-	fi	
-}
+# function ros_source_env() 
+# {
+# 	if [ -f "$1" ]; then
+# 		echo "sourcing   $1"
+# 		source "$1"
+# 	else
+# 		echo "notfound   $1"
+# 	fi	
+# }
 
-if [[ "$ROS_DISTRO" == "melodic" || "$ROS_DISTRO" == "noetic" ]]; then
-	ros_source_env "/opt/ros/$ROS_DISTRO/setup.bash"
-else
-	ros_source_env "$ROS_ROOT/install/setup.bash"
+# if [[ "$ROS_DISTRO" == "melodic" || "$ROS_DISTRO" == "noetic" ]]; then
+# 	ros_source_env "/opt/ros/$ROS_DISTRO/setup.bash"
+# else
+# 	ros_source_env "$ROS_ROOT/install/setup.bash"
 
-	#echo "ROS_PACKAGE_PATH   $ROS_PACKAGE_PATH"
-	#echo "COLCON_PREFIX_PATH $COLCON_PREFIX_PATH"
-	#echo "AMENT_PREFIX_PATH  $AMENT_PREFIX_PATH"
-	#echo "CMAKE_PREFIX_PATH  $CMAKE_PREFIX_PATH"
-fi
+# 	#echo "ROS_PACKAGE_PATH   $ROS_PACKAGE_PATH"
+# 	#echo "COLCON_PREFIX_PATH $COLCON_PREFIX_PATH"
+# 	#echo "AMENT_PREFIX_PATH  $AMENT_PREFIX_PATH"
+# 	#echo "CMAKE_PREFIX_PATH  $CMAKE_PREFIX_PATH"
+# fi
 
-echo "ROS_DISTRO $ROS_DISTRO"
-echo "ROS_ROOT   $ROS_ROOT"
+# echo "ROS_DISTRO $ROS_DISTRO"
+# echo "ROS_ROOT   $ROS_ROOT"
 
+# exec "$@"
+
+#!/bin/bash
+set -e
+# setup ros2 environment
+source /.bashrc
+source "$ROS2_WS/install/setup.bash"
 exec "$@"
